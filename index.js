d4d0d38b2321f1158911218f9e25c6650c6e03e4
Jessica <91101093+Jessicm92@users.noreply.github.com>
2022-11-15 17:16:13 -0800

    Added Bootstrap
    Several changes like background color, fonts and added Bootstrap

--- index.js
+++ index.js
@@ -1,58 +1,44 @@
 $(document).ready(function() {
 
 
-      //$('h2').hide(100);
-      //$('h5').hide(100);
-      //$(".searchdiv").hide(100);
-      //$('img').hide(100);
-      $('#aboutp').hide(100);
-      $('#abouth2').hide(100);
-      $("#comedyjq").hide();
-      $("#romancejq").hide();
-      $("#scifijq").hide();
-      $("#horrorjq").hide();
-      $("#actionjq").hide();
-      $("#fantasyjq").hide();
-
-      $('.aboutlink').click(function() {
-        //event.preventDefault()
-        $('#abouth2').show();
-        $('#aboutp').show();
-        $("#homeparent").hide();
-        $("#homeselect").hide();
-
-      });
-
-      $(".homediv").click(function() {
-        location.reload(true);
-      });
-
-      $('.movies').on('change', function() {
-        $(".moviesection").hide();
-        switch(this.value){
-          case "Comedy":
-          $("#comedyjq").show("slow");
-          break;
-          case "Romance":
-          $("#romancejq").show("slow");
-          break
-          case "Sci-fi":
-          $("#scifijq").show("slow");
-          break;
-          case "Horror":
-          $("#horrorjq").show("slow");
-          break;
-          case "Action":
-          $("#actionjq").show("slow");
-          break;
-          case "Fantasy":
-          $("#fantasyjq").show("slow");
-          break;
-        }
+$("#romance, #scifi, #horror, #action, #fantasy").hide();
+$(".romance-header, .scifi-header, .horror-header, .action-header, .fantasy-header").hide();
 
 
+// $("#navbarNav").click(function() {
+//   location.reload(true);
+// });
 
-        console.log("h")
-      });
+$('#inputGroupSelect01').on('change', function() {
+  $("#comedy, .comedy-header").hide();
+  $("#romance, .romance-header").hide();
+  $("#scifi, .scifi-header").hide();
+  $("#horror, .horror-header").hide();
+  $("#action, .action-header").hide();
+  $("#fantasy, .fantasy-header").hide();
+  switch(this.value){
+    case "Comedy":
+    $("#comedy, .comedy-header").show();
+    break
+    case "Romance":
+      $("#romance, .romance-header").show();
+    break
+    case "Sci-fi":
+      $("#scifi, .scifi-header").show();
+    break;
+    case "Horror":
+      $("#horror, .horror-header").show();
+    break;
+    case "Action":
+      $("#action, .action-header").show();
+    break;
+    case "Fantasy":
+      $("#fantasy, .fantasy-header").show();
+    break;
+  }
+
+  console.log ("hola")
 
 });
+
+  });
